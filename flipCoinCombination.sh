c0e66621601adddec1ff2dee518e1decb9564bd3
Mayank Singh <smayank748@gmail.com>
2021-11-13 09:40:21 +0530

    [Mayank] storing the data in dictionary

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,1 +1,12 @@
-echo $(( $RANDOM%2 ))
+declare -A singlet
+isTrue=true
+flipStatus=0
+counter=0
+while [ $isTrue = true ]
+do
+flipStatus=$(( $RANDOM%2 ))
+singlet[ $counter ]=$flipStatus
+read -p "Do you want to flip coin again. If yes then type true " isTrue
+((counter++))
+done
+echo "All values "${singlet[@]}
